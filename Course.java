357dc6d20d5737da68533c8bf9a8a861579426d0
Saloni0211 <saloni7143116@gmail.com>
2025-05-20 00:22:27 +0530

    Course.java

--- Course.java
+++ Course.java
@@ -1,1 +1,15 @@
+package com.example.university.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    private String code;
+    private String name;
+    private String instructor;
+
+    // Getters and Setters
+}
 
